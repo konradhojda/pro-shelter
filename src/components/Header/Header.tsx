a0bdc572ee4606229d3e1bec7669af57fe7c63d5
konradhojda <konrad.hojda@gmail.com>
2023-04-15 21:31:57 +0200

    feat(core): init

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+interface Props {
+  isVisible?: boolean;
+}
+
+const nav = [{ name: "O nas", href: "/about" }];
+
+export const Header = ({ ...rest }: Props) => {
+  return <header>items</header>;
+};
---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { Header } from "@/components/Header";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import React from "react";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <React.Fragment>
+      <Header />
+      <Component {...pageProps} />
+      <footer>FOoter</footer>
+    </React.Fragment>
+  );
+}
